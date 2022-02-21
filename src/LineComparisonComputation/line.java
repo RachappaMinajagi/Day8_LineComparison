1d596b32cb3810e10423c346ebd1df5ac218579d
Rachappa-Minajagi <m.rachoti142@gmail.com>
2022-02-21 23:12:38 +0530

    Displaying well come message

---
+++ src/LineComparisonComputation/line.java
@@ -0,0 +1,10 @@
+package LineComparisonComputation;
+
+public class line {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation Program on Main Branch");
+
+	}
+
+}
