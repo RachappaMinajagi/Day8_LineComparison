52cf0144b538ddae3f1407d241380cbdd6fdaf32
Rachappa-Minajagi <m.rachoti142@gmail.com>
2022-02-21 23:39:38 +0530

    Using Cartesian System calculate its length

--- src/LineComparisonComputation/line.java
+++ src/LineComparisonComputation/line.java
@@ -3,8 +3,22 @@ package LineComparisonComputation;
 public class line {
 
 	public static void main(String[] args) {
+		
 		System.out.println("Welcome to Line Comparison Computation Program on Main Branch");
+		        int x1 = 2;
+		        int x2 = 4;
+		        int y1 = 5;
+		        int y2 = 10;
 
+		        System.out.println("A Length as 2 points : " + "("+x1+ "," +y1+ ")" + "and" + "(" +x2+ "," +y2+ ")");
+		        double LenthLine = Math.sqrt((x2 - x1) * (x2-x1)) + ((y2-y1) * (y2-y1));
+		        System.out.println("Length of a line : "+ LenthLine);
+
+		    
+
+		
+		
 	}
-
 }
+
+	
